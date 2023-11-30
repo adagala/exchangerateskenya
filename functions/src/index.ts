159f25b23203815a448f66b7800f049c100a2593
Henry Adagala <adagalahenry@gmail.com>
2023-11-30 13:03:08 +0300

    tweet new exchange rates

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,9 @@
 import { onRequest } from 'firebase-functions/v2/https';
 import { onSchedule } from 'firebase-functions/v2/scheduler';
+import { TwitterApi } from 'twitter-api-v2';
 import * as admin from 'firebase-admin';
+import { formatDate } from './utils';
+
 admin.initializeApp();
 
 export const getRates = onRequest(
@@ -32,7 +35,13 @@ export const getDailyRates = onSchedule(
   {
     schedule: '0 9 * * *',
     maxInstances: 5,
-    secrets: ['FIXER_API_KEY'],
+    secrets: [
+      'FIXER_API_KEY',
+      'TWITTER_APP_KEY',
+      'TWITTER_APP_SECRET',
+      'TWITTER_ACCESS_SECRET',
+      'TWITTER_ACCESS_TOKEN',
+    ],
     timeZone: 'Africa/Nairobi',
   },
   async () => {
@@ -54,6 +63,17 @@ export const getDailyRates = onSchedule(
     };
 
     await admin.firestore().doc(`rates/${baseAsKenya.date}`).set(baseAsKenya);
+
+    const twitterClient = new TwitterApi({
+      appKey: process.env.TWITTER_APP_KEY as string,
+      appSecret: process.env.TWITTER_APP_SECRET as string,
+      accessSecret: process.env.TWITTER_ACCESS_SECRET as string,
+      accessToken: process.env.TWITTER_ACCESS_TOKEN as string,
+    });
+    const displayDate = formatDate(new Date(baseAsKenya.date));
+    const tweetText = `Today's exhange rates against the Kenyan shilling (${displayDate}): \n\nUSD ($): ${baseAsKenya.rates.USD}\nEUR (€): ${baseAsKenya.rates.EUR}\nGBP (£): ${baseAsKenya.rates.GBP}\nJPY (¥): ${baseAsKenya.rates.JPY}`;
+    await twitterClient.v2.tweet(tweetText);
+
     return;
   },
 );
