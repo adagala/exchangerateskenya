b87d1d4fd649983a07e46404c7bf4f3f61f6bc34
Henry Adagala <adagalahenry@gmail.com>
2023-12-06 10:59:57 +0300

    update: format time

--- functions/src/utils.ts
+++ functions/src/utils.ts
@@ -35,5 +35,7 @@ const nthDate = (d: number) => {
 export const formatTime = (date: Date) => {
   const hours = date.getHours();
   const minutes = date.getMinutes();
-  return `${hours}:${minutes}`;
+  const formattedHours = hours < 10 ? `0${hours}` : `${hours}`;
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+  return `${formattedHours}:${formattedMinutes}`;
 };
